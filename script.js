780deaa0c7caf8fc1764e33bf260ac82169cfb0a
Filip <filipwedzicha@gmail.com>
2023-01-13 11:47:19 +0000

    Optimize site loading slightly

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const date = new Date();
+document.addEventListener("load", () => {
+  const date = new Date();
 
-const monthEmoji = [
-  "🥂",
-  "💝",
-  "",
-  "🐣",
-  "🎂",
-  "",
-  "",
-  "",
-  "🏫",
-  "🎃",
-  "🎆",
-  "🎅",
-];
-const emoji = monthEmoji.at(date.getMonth());
+  const monthEmoji = [
+    "🥂",
+    "💝",
+    "",
+    "🐣",
+    "🎂",
+    "",
+    "",
+    "",
+    "🏫",
+    "🎃",
+    "🎆",
+    "🎅",
+  ];
+  const emoji = monthEmoji.at(date.getMonth());
 
-const title = document.getElementsByTagName("title")[0];
-title.text += ` ${emoji}`;
+  const title = document.getElementsByTagName("title")[0];
+  title.text += ` ${emoji}`;
+
+})
